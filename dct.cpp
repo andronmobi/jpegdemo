968cc3412707e5760d0c803b188a46670dcf85a7
Andrei Mandychev <andrei.mandychev@parrot.com>
2015-03-20 19:50:44 +0100

    [DEV] Quantization is added

--- dct.cpp
+++ dct.cpp
@@ -13,6 +13,7 @@
 #define CONSTA
 
 #ifdef CONSTA
+#if 0
 static const float A[] = {
     51, 52, 51, 50, 50, 52, 50, 52,
     51, 52, 51, 51, 50, 52, 52, 51,
@@ -23,23 +24,34 @@ static const float A[] = {
     51, 52, 51, 50, 52, 50, 52, 50,
     50, 51, 52, 52, 50, 51, 52, 51,
 };
-#endif
-
-/*
+#else
 static const float A[] = {
     -122,   49,     66,     41,     41,     43,     40,     38,
     -121,   49,     31,     45,     35,     50,     41,     24,
     -122,   40,     45,     105,    31,     -66,    18,     87,
-    -94,    52,     42,     47,     -122,   -122,   8,      51
+    -94,    52,     42,     47,     -122,   -122,   8,      51,
     -119,   -23,    53,     51,     45,     70,     61,     42,
     -64,    -122,   -25,    -26,    33,      15,    6,      12,
     -76,    -80,    -64,    -122,   53,     64,     38,     -122,
     -78,    -74,    -84,    -122,   57,     43,     41,     -53,
-};*/
+};
+#endif
+#endif
+
+static const float quantizCoefs[] = {
+    16, 11, 10, 16, 24, 40, 51, 61,
+    12, 12, 14, 19, 26, 58, 60, 55,
+    14, 13, 16, 24, 40, 57, 69, 56,
+    14, 17, 22, 29, 51, 87, 80, 62,
+    18, 22, 37, 56, 68, 109, 103, 77,
+    24, 35, 55, 64, 81, 104, 113, 92,
+    49, 64, 78, 87, 103, 121, 120, 101,
+    72, 92, 95, 98, 112, 100, 103, 99,
+};
 
 static gsl_matrix_float* matrixU;
 
-void print_matrix(const gsl_matrix_float* m) {
+void print_matrix_float(const gsl_matrix_float* m) {
     for (unsigned int i = 0; i < m->size1; i++) {
         for (unsigned int j = 0; j < m->size2; j++) {
             printf("%.3f\t", gsl_matrix_float_get(m, i, j));
@@ -47,6 +59,34 @@ void print_matrix(const gsl_matrix_float* m) {
         printf("\n");
     }
     printf("\n");
+}
+
+void print_matrix_int(const gsl_matrix_int* m) {
+    for (unsigned int i = 0; i < m->size1; i++) {
+        for (unsigned int j = 0; j < m->size2; j++) {
+            printf("%d\t", gsl_matrix_int_get(m, i, j));
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void convert_matrix_float_to_int(const gsl_matrix_float* im, gsl_matrix_int* om) {
+    for (unsigned int i = 0; i < im->size1; i++) {
+        for (unsigned int j = 0; j < im->size2; j++) {
+            int elem = lroundf(gsl_matrix_float_get(im, i, j));
+            gsl_matrix_int_set(om, i, j, elem);
+        }
+    }
+}
+
+void copy_matrix_int_to_float(const gsl_matrix_int* im, gsl_matrix_float* om) {
+    for (unsigned int i = 0; i < im->size1; i++) {
+        for (unsigned int j = 0; j < im->size2; j++) {
+            float elem = (float) gsl_matrix_int_get(im, i, j);
+            gsl_matrix_float_set(om, i, j, elem);
+        }
+    }
 }
 
 void create_dct() {
@@ -71,7 +111,7 @@ void compute(const gsl_matrix_float* im, gsl_matrix_float* om) {
     // C = U * A, where A is input matrix
     gsl_matrix_float* matrixC = gsl_matrix_float_alloc(8, 8);
     gsl_blas_sgemm(CblasNoTrans, CblasNoTrans, 1., matrixU, im, 0., matrixC);
-    print_matrix(matrixC);
+    print_matrix_float(matrixC);
     // B = C * U', where B is output matrix
     gsl_blas_sgemm(CblasNoTrans, CblasTrans, 1., matrixC, matrixU, 0., om);
     gsl_matrix_float_free(matrixC);
@@ -81,7 +121,7 @@ void inv_compute(const gsl_matrix_float* im, gsl_matrix_float* om) {
     // C = U' * B, where B is input matrix
     gsl_matrix_float* matrixC = gsl_matrix_float_alloc(8, 8);
     gsl_blas_sgemm(CblasTrans, CblasNoTrans, 1., matrixU, im, 0., matrixC);
-    print_matrix(matrixC);
+    print_matrix_float(matrixC);
     // A = C * U, where B is output matrix
     gsl_blas_sgemm(CblasNoTrans, CblasNoTrans, 1., matrixC, matrixU, 0., om);
     gsl_matrix_float_free(matrixC);
@@ -102,7 +142,7 @@ int main (int argc, char *argv[]) {
     gsl_matrix_float_set_all(matrixA, 100);
 #endif
     printf("matrix A\n");
-    print_matrix(matrixA);
+    print_matrix_float(matrixA);
 
     // Create output matrix B
     gsl_matrix_float* matrixB = gsl_matrix_float_alloc(8, 8);
@@ -110,13 +150,30 @@ int main (int argc, char *argv[]) {
     // B = U * A * U'
     compute(matrixA, matrixB);
     printf("matrix B = U * A * U'\n");
-    print_matrix(matrixB);
+    print_matrix_float(matrixB);
+
+    // Quantization
+    _gsl_matrix_float_const_view viewCoefs = gsl_matrix_float_const_view_array(quantizCoefs, 8, 8);
+    gsl_matrix_float* matrixCoefs = &viewCoefs.matrix;
+    gsl_matrix_float_div_elements(matrixB, matrixCoefs);
+    //print_matrix_float(matrixB);
+    gsl_matrix_int* matrixQ = gsl_matrix_int_alloc(8, 8);
+    convert_matrix_float_to_int(matrixB, matrixQ);
+    printf("quantized matrix B\n");
+    print_matrix_int(matrixQ);
+
+    // Dequantization
+    copy_matrix_int_to_float(matrixQ, matrixB);
+    gsl_matrix_float_mul_elements(matrixB, matrixCoefs);
+    printf("dequantized matrix B\n");
+    print_matrix_float(matrixB);
 
     // Create output matrix A, restored
     gsl_matrix_float* matrixAR = gsl_matrix_float_alloc(8, 8);
     inv_compute(matrixB, matrixAR);
     printf("matrix A = U' * B * U\n");
-    print_matrix(matrixAR);
+    print_matrix_float(matrixAR);
+
 
     destroy_dct();
 
@@ -125,6 +182,7 @@ int main (int argc, char *argv[]) {
 #endif
     gsl_matrix_float_free(matrixAR);
     gsl_matrix_float_free(matrixB);
+    gsl_matrix_int_free(matrixQ);
 
     return 0;
 }
